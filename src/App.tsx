9cb22806c0842794c9c6e2a4b61f4bf1b694ad1a
Ferv0r2 <amlk31255@gmail.com>
2025-10-15 01:11:24 +0900

    env: 국제화 & alias 설정 (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
+import I18nExample from '@components/I18nExample';
 import { Application, extend } from '@pixi/react';
-import { Container, Graphics, Text } from 'pixi.js';
+import { Container } from 'pixi.js';
 
 import { GameContainer } from './components/GameContainer';
 
 // extend tells @pixi/react what Pixi.js components are available
 extend({
   Container,
-  Graphics,
   Text,
 });
 
 export default function App() {
   return (
-    // We'll wrap our components with an <Application> component to provide
-    // the Pixi.js Application context
-    <Application background={'#1a1a2e'} resizeTo={window}>
-      <GameContainer />
-    </Application>
+    <>
+      <I18nExample />
+      {/* We'll wrap our components with an <Application> component to provide
+          the Pixi.js Application context */}
+      <Application background={'#1a1a2e'} resizeTo={window}>
+        <GameContainer />
+      </Application>
+    </>
   );
 }
